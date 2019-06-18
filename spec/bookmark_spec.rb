2de374fd78905b8738f3c4ca82243ad4973717bc
Alexis Josephides <ajosephides@gmail.com>
2019-06-18 15:33:40 +0100

    altered test websites to match databases

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,9 +5,9 @@ describe Bookmark do
     it 'returns a list of bookmarks' do
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("https://google.com")
-      expect(bookmarks).to include("https://google.com")
-      expect(bookmarks).to include("https://cnn.com")
+      expect(bookmarks).to include("http://www.makersacademy.com")
+      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks).to include("http://www.destroyallsoftware.com")
     end
   end 
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -10,8 +10,8 @@ feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     visit ('/bookmarks')
 
-    expect(page).to have_content "https://google.com"
-    expect(page).to have_content "https://google.com"
-    expect(page).to have_content "https://cnn.com"
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
